4798e9c98159a4292389526804652e7c2927d646
Satvik Shukla <satvik.shukla@warnermedia.com>
2022-10-21 17:30:08 -0700

    update version

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.warnermedia"
             artifactId = "rulesengine"
-            version = "1.0.0"
+            version = "1.1.0"
 
             from(components["java"])
         }
