f58bc4a0c10a74dca085dd82b4aeb199e1f7f0eb
Satvik Shukla <61213829+satvik-s@users.noreply.github.com>
2022-10-27 11:17:47 -0700

    chore: default value for condition joiner (#11)

--- kotlin/src/main/kotlin/com/warnermedia/rulesengine/RuleOptions.kt
+++ kotlin/src/main/kotlin/com/warnermedia/rulesengine/RuleOptions.kt
@@ -1,7 +1,7 @@
 package com.warnermedia.rulesengine
 
 class RuleOptions(
-    val conditionJoiner: ConditionJoiner,
+    val conditionJoiner: ConditionJoiner = ConditionJoiner.AND,
     val enabled: Boolean = true,
     val startTime: Int = Int.MIN_VALUE,
     val endTime: Int = Int.MAX_VALUE,
