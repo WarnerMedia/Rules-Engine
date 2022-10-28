23541e8ddf6eadc6d213f25eb01a3107f4491c57
Satvik Shukla <61213829+satvik-s@users.noreply.github.com>
2022-10-27 19:12:36 -0700

    chore: add util functions (#15)

--- kotlin/src/main/kotlin/com/warnermedia/rulesengine/ExitCriteria.kt
+++ kotlin/src/main/kotlin/com/warnermedia/rulesengine/ExitCriteria.kt
@@ -4,4 +4,12 @@ sealed class ExitCriteria {
     class EarlyExit(val ruleResult: RuleResult) : ExitCriteria()
 
     class NormalExit : ExitCriteria()
+
+    fun isEarlyExit(): Boolean {
+        return this is EarlyExit
+    }
+
+    fun isNormalExit(): Boolean {
+        return this is NormalExit
+    }
 }
