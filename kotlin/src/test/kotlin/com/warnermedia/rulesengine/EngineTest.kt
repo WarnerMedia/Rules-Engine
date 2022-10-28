fec72f982dfdaa2eeeb777c84bbcdf74d77f848e
Satvik Shukla <satvik.shukla@warnermedia.com>
2022-10-27 23:12:34 -0700

    build fix

--- kotlin/src/test/kotlin/com/warnermedia/rulesengine/EngineTest.kt
+++ kotlin/src/test/kotlin/com/warnermedia/rulesengine/EngineTest.kt
@@ -21,13 +21,13 @@ internal class EngineTest {
     @Test
     fun testSuccessfulResult() {
         val result = engine.evaluate(hashMapOf("temperature" to 75, "rainfall" to 0))
-        assertEquals(arrayListOf(RuleResult.Success("good-weather", "good-weather-day")), result.ruleEvaluations)
+        assertEquals<ArrayList<RuleResult>>(arrayListOf(RuleResult.Success("good-weather", "good-weather-day")), result.ruleEvaluations)
     }
 
     @Test
     fun testFailureResult() {
         val result = engine.evaluate(hashMapOf("temperature" to 60, "rainfall" to 0))
-        assertEquals(arrayListOf(RuleResult.Failure("good-weather", "work-from-home-day")), result.ruleEvaluations)
+        assertEquals<ArrayList<RuleResult>>(arrayListOf(RuleResult.Failure("good-weather", "work-from-home-day")), result.ruleEvaluations)
     }
 
     @Test
@@ -36,6 +36,6 @@ internal class EngineTest {
 
         val engineFromFile = Engine.readFromFile("engine.json")
         val result = engineFromFile.evaluate(hashMapOf("temperature" to 75, "rainfall" to 0))
-        assertEquals(arrayListOf(RuleResult.Success("good-weather", "good-weather-day")), result.ruleEvaluations)
+        assertEquals<ArrayList<RuleResult>>(arrayListOf(RuleResult.Success("good-weather", "good-weather-day")), result.ruleEvaluations)
     }
 }
