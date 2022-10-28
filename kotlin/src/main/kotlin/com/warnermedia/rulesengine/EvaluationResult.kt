1c868c4cde1486619b18b330f518b7bb3fe4a99b
Satvik Shukla <satvik.shukla@warnermedia.com>
2022-10-27 23:04:22 -0700

    nit

--- kotlin/src/main/kotlin/com/warnermedia/rulesengine/EvaluationResult.kt
+++ kotlin/src/main/kotlin/com/warnermedia/rulesengine/EvaluationResult.kt
@@ -1,6 +1,6 @@
 package com.warnermedia.rulesengine
 
-class EvaluationResult(val ruleEvaluations: List<RuleResult>, val exitCriteria: ExitCriteria) {
+class EvaluationResult(val ruleEvaluations: ArrayList<RuleResult>, val exitCriteria: ExitCriteria) {
     override fun toString(): String {
         return ruleEvaluations.toString()
     }
