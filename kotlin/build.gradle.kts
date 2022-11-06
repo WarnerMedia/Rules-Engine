59e2db0afae3d5ed173a68706295af5a6a4a735a
Satvik Shukla <satvik.shukla@outlook.com>
2022-11-06 12:36:01 -0800

    update gradle v

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,6 @@
+tasks.wrapper {
+    gradleVersion = "7.5.1"
+    // You can either download the binary-only version of Gradle (BIN) or
+    // the full version (with sources and documentation) of Gradle (ALL)
+    distributionType = Wrapper.DistributionType.BIN
+}
