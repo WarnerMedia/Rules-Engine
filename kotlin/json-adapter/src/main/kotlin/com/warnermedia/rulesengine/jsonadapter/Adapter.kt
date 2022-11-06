6ac61addee41741bd872641df41e996e2472d54e
Satvik Shukla <satvik.shukla@outlook.com>
2022-11-06 13:14:49 -0800

    update json adapter

--- kotlin/json-adapter/src/main/kotlin/com/warnermedia/rulesengine/jsonadapter/Adapter.kt
+++ kotlin/json-adapter/src/main/kotlin/com/warnermedia/rulesengine/jsonadapter/Adapter.kt
@@ -4,27 +4,25 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.warnermedia.rulesengine.core.Engine
 import java.io.File
 
-class Adapter {
-    companion object {
-        private val mapper = jacksonObjectMapper()
+object Adapter {
+    private val mapper = jacksonObjectMapper()
 
-        @JvmStatic
-        fun readFromFile(path: String): Engine {
-            val engineStringRepresentation = File(path).readText(Charsets.UTF_8)
-            return readFromString(engineStringRepresentation)
-        }
+    @JvmStatic
+    fun readFromFile(path: String): Engine {
+        val engineStringRepresentation = File(path).readText(Charsets.UTF_8)
+        return readFromString(engineStringRepresentation)
+    }
 
-        @JvmStatic
-        fun saveToFile(engine: Engine, path: String) {
-            File(path).writeText(engineToString(engine), Charsets.UTF_8)
-        }
+    @JvmStatic
+    fun saveToFile(engine: Engine, path: String) {
+        File(path).writeText(engineToString(engine), Charsets.UTF_8)
+    }
 
-        private fun engineToString(engine: Engine): String {
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(engine)
-        }
+    private fun engineToString(engine: Engine): String {
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(engine)
+    }
 
-        private fun readFromString(engineStringRepresentation: String): Engine {
-            return mapper.readValue(engineStringRepresentation, Engine::class.java)
-        }
+    private fun readFromString(engineStringRepresentation: String): Engine {
+        return mapper.readValue(engineStringRepresentation, Engine::class.java)
     }
 }
