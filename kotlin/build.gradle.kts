3f8db6fe6de8498746068cc39b28dafcd7f3bfa7
Satvik Shukla <satvik.shukla@outlook.com>
2022-12-05 19:33:38 -0800

    allow multimodule dokka

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,3 +1,11 @@
+plugins {
+    id("org.jetbrains.dokka")
+}
+
+repositories {
+    mavenCentral()
+}
+
 tasks.wrapper {
     gradleVersion = "7.5.1"
     distributionType = Wrapper.DistributionType.BIN
