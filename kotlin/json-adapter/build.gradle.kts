68d3ae82d753ac148e6dd2052966f6ee0791bbcd
Satvik Shukla <61213829+satvik-s@users.noreply.github.com>
2022-12-21 16:26:12 -0800

    chore: remove buildsrc (#100)

--- kotlin/json-adapter/build.gradle.kts
+++ kotlin/json-adapter/build.gradle.kts
@@ -1,8 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val packageGroupId: String by project
+val packageVersionId: String by project
+
+group = packageGroupId
+version = packageVersionId
+
 plugins {
-    `kotlin-library-conventions`
-    `kotlin-dokka-conventions`
     kotlin("jvm")
     id("org.jetbrains.dokka")
+    `maven-publish`
+    `java-library`
 }
 
 repositories {
@@ -12,13 +20,32 @@ repositories {
 dependencies {
     implementation(project(":core"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.1")
+    testImplementation(kotlin("test"))
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/warnermedia/rules-engine")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
             artifactId = "rulesengine-jsonadapter"
             from(components["java"])
         }
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
